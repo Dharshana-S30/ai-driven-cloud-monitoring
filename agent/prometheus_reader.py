86265ad151373149521de6fe05b9f733651e5db0
Dharshana-S30 <dharshanapapiyan@gmail.com>
2026-05-10 21:43:19 +0530

    Update prometheus_reader.py

--- agent/prometheus_reader.py
+++ agent/prometheus_reader.py
@@ -1,62 +1,77 @@
 import requests
-import time
 
-PROMETHEUS_URL = "http://13.235.50.151:32250/api/v1/query"
+
+PROMETHEUS_URL = (
+    "http://localhost:9090"
+)
+
+
 def query_prometheus(query):
-    try:
-        response = requests.get(
-            PROMETHEUS_URL,
-            params={"query": query}
-        )
 
-        data = response.json()
+    url = (
+        f"{PROMETHEUS_URL}/api/v1/query"
+    )
 
-        print(data)
-        return data["data"]["result"]
+    response = requests.get(
+        url,
+        params={"query": query}
+    )
 
-    except Exception as e:
-        print("Prometheus Error:", e)
-        return []
+    data = response.json()
 
+    return data["data"]["result"]
+
+
+# =========================================
+# CPU USAGE
+# =========================================
 
 def get_cpu_usage():
-    query = '100 - (avg by(instance)(rate(node_cpu_seconds_total{mode="idle"}[2m])) * 100)'
-    result = query_prometheus(query)
 
-    print("\nCPU Usage:")
-    print(result)
+    query = (
+        '100 - (avg by(instance)'
+        '(rate(node_cpu_seconds_total'
+        '{mode="idle"}[1m])) * 100)'
+    )
 
+    return query_prometheus(query)
+
+
+# =========================================
+# MEMORY USAGE
+# =========================================
 
 def get_memory_usage():
-    query = '(1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)) * 100'
-    result = query_prometheus(query)
 
-    print("\nMemory Usage:")
-    print(result)
+    query = (
+        '(1 - (node_memory_MemAvailable_bytes '
+        '/ node_memory_MemTotal_bytes)) * 100'
+    )
+
+    return query_prometheus(query)
 
 
-def get_pod_restarts():
-    query = 'kube_pod_container_status_restarts_total'
-    result = query_prometheus(query)
+# =========================================
+# POD RESTARTS
+# =========================================
 
-    print("\nPod Metrics:")
-    print(result)
+def get_pod_metrics():
 
+    query = (
+        'kube_pod_container_status_restarts_total'
+    )
+
+    return query_prometheus(query)
+
+
+# =========================================
+# REQUEST RATE
+# =========================================
 
 def get_request_rate():
-    query = 'rate(container_network_receive_bytes_total[1m])'
-    result = query_prometheus(query)
 
-    print("\nRequest Rate:")
-    print(result)
+    query = (
+        'rate(container_network_receive_bytes_total[1m])'
+    )
 
-
-while True:
-    print("\n===== PROMETHEUS METRICS =====")
-
-    get_cpu_usage()
-    get_memory_usage()
-    get_pod_restarts()
-    get_request_rate()
-
-    time.sleep(60)
+    return query_prometheus(query)
